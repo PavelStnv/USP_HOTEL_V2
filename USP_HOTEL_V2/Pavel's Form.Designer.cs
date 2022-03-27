02087283241acd36aeeb0b3b8d249b770be347c4
Pavel Stoyanov <pavel.stoyanov.bg@gmail.com>
2022-03-27 16:15:51 +0300

    Добавен datagridview за визуализиране на заетите помещения

--- USP_HOTEL_V2/Pavel's Form.Designer.cs
+++ USP_HOTEL_V2/Pavel's Form.Designer.cs
@@ -29,12 +29,35 @@ namespace USP_HOTEL_V2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(386, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Pavel_s_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(410, 281);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Pavel_s_Form";
             this.Text = "Pavel_s_Form";
+            this.Load += new System.EventHandler(this.Pavel_s_Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- USP_HOTEL_V2/Pavel's Form.cs
+++ USP_HOTEL_V2/Pavel's Form.cs
@@ -5,14 +5,50 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+
 
 namespace USP_HOTEL_V2
 {
     public partial class Pavel_s_Form : Form
     {
+        string ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + Directory.GetCurrentDirectory() + "\\DB_Tema17.mdb";
+        OleDbConnection dbConnect = new OleDbConnection();
+
         public Pavel_s_Form()
         {
             InitializeComponent();
+        }
+
+        private void Pavel_s_Form_Load(object sender, EventArgs e)
+        {
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+
+            dataGridView1.MultiSelect = false;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            InsertData();
+
+        }
+
+        private void InsertData()
+        {
+            string mySelect = "SELECT * FROM ROOMS WHERE ROOM_STATUS = 1";
+
+            dbConnect.ConnectionString = ConnectionString;
+            dbConnect.Open();
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(mySelect, dbConnect);
+
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+            dbConnect.Close();
         }
     }
 }
