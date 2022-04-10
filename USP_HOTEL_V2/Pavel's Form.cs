97dfc4b0b8e7a644b37568271d290921748b1d6f
Pavel Stoyanov <pavel.stoyanov.bg@gmail.com>
2022-04-10 11:48:15 +0300

    Добавена функционалност за добавяне на резервация

--- USP_HOTEL_V2/Pavel's Form.cs
+++ USP_HOTEL_V2/Pavel's Form.cs
@@ -25,6 +25,7 @@ namespace USP_HOTEL_V2
         {
             this.MinimizeBox = false;
             this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
 
             dataGridView1.MultiSelect = false;
             dataGridView1.ReadOnly = true;
@@ -32,7 +33,6 @@ namespace USP_HOTEL_V2
             dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
 
             InsertData();
-
         }
 
         private void InsertData()
@@ -40,7 +40,7 @@ namespace USP_HOTEL_V2
             string mySelect = "SELECT ROOMS.ID, ROOM_TYPES.ROOM_TYPE, ROOM_STATUSES.ROOM_STATUS FROM(( ROOMS " +
                 "INNER JOIN ROOM_TYPES ON ROOMS.ROOM_TYPE = ROOM_TYPES.ID) " +
                 "INNER JOIN ROOM_STATUSES ON ROOMS.ROOM_STATUS = ROOM_STATUSES.ID)" +
-                "ORDER BY ROOM_STATUSES.ROOM_STATUS DESC";
+                "ORDER BY ROOMS.ID";
 
             dbConnect.ConnectionString = ConnectionString;
             dbConnect.Open();
@@ -51,6 +51,94 @@ namespace USP_HOTEL_V2
             adapter.Fill(dt);
 
             dataGridView1.DataSource = dt;
+            dbConnect.Close();
+        }
+
+        private void btnCreateReservation_Click(object sender, EventArgs e)
+        {
+            int selectedRowCount = dataGridView1.Rows.GetRowCount(DataGridViewElementStates.Selected);
+
+            switch(selectedRowCount)
+            {
+                case 0:
+                    MessageBox.Show("Моля изберете помещение.", "Грешка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+                case 1:
+                    {
+                        foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+                        {
+                            string strRoomStatus = row.Cells[2].Value.ToString();
+
+                            if(strRoomStatus == "Заето")
+                            {
+                                MessageBox.Show("Помещението е заето. Моля изберете свободно такова.", "Грешка",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                break;
+                            }
+
+                            DateTime today = DateTime.Now.Date;
+                            DateTime from = dateTimePickerFrom.Value.Date;
+                            DateTime to = dateTimePickerTo.Value.Date;
+
+                            if(DateTime.Compare(today, from) > 0)
+                            {
+                                MessageBox.Show("Най-ранната дата за 'От' може да е днешната.", "Грешка",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                break;
+                            }
+
+                            if (DateTime.Compare(from, to) > 0)
+                            {
+                                MessageBox.Show("Дата 'От' не може да е по-голяма от дата 'До'.", "Грешка",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                break;
+                            }
+
+                            AddRecordInReservations(row.Cells[0].Value.ToString(), from, to);
+                            MarkSelectedAsTaken(row.Cells[0].Value.ToString());
+                            InsertData();
+                        }
+                    }
+                    break;
+                default:
+                    MessageBox.Show("Моля изберете само едно помещение.", "Грешка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+            }
+        }
+
+        void AddRecordInReservations(string roomID, DateTime from, DateTime to)
+        {
+            string myInsert = "INSERT INTO RESERVATIONS VALUES('" + roomID + "', '" + from.ToString() +
+                "', '" + to.ToString() + "')";
+
+            dbConnect.ConnectionString = ConnectionString;
+
+            OleDbCommand dbCommand = new OleDbCommand(myInsert, dbConnect);
+            dbConnect.Open();
+
+            dbCommand.CommandText = myInsert;
+            dbCommand.Connection = dbConnect;
+            dbCommand.ExecuteNonQuery();
+
+            MessageBox.Show("Успешно добавяне на резервация.", "Успех",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            dbConnect.Close();
+        }
+
+        void MarkSelectedAsTaken(string roomID)
+        {
+            string myUpdate = "UPDATE ROOMS SET ROOM_STATUS = 1 WHERE ID = " + roomID;
+
+            dbConnect.ConnectionString = ConnectionString;
+
+            OleDbCommand dbCommand = new OleDbCommand(myUpdate, dbConnect);
+            dbConnect.Open();
+
+            dbCommand.CommandText = myUpdate;
+            dbCommand.Connection = dbConnect;
+            dbCommand.ExecuteNonQuery();
+
             dbConnect.Close();
         }
     }
